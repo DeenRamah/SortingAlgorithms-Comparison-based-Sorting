77a911a649f9990e18f63ee574d3fa8ed5914949
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-22 22:14:43 +0300

    Create bubble.ts

---
+++ bubble.ts
@@ -0,0 +1,13 @@
+function bab(arr: number[]):number{
+  let n = arr.length;
+  for(let i=0; i<n-1; i++){
+    for(let j=0; j<n-1; j++){
+      if(arr[j]> arr[j+1]){
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bab([64,34,25,12,22,11,90]));

67e01e3a47ef17f3a210cf466e9b726bda5a5d27
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-10-13 19:08:24 +0300

    Create ins.js

---
+++ ins.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  let n = arr.length;
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}

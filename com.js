240992e81ca18b863e4f489329ea84c262671dd7
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-10-13 19:08:08 +0300

    Create com.js

---
+++ com.js
@@ -0,0 +1,13 @@
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]; // Swap
+  }
+  return arr;
+}

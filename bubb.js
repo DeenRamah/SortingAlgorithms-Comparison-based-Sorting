d064512105a31883ba7b3f9cbff8ed5019e36ee2
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-10-13 19:07:51 +0300

    Create bubb.js

---
+++ bubb.js
@@ -0,0 +1,11 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // Swap
+      }
+    }
+  }
+  return arr;
+}
